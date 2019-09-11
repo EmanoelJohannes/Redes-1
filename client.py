3c9541a65122b7c12d16f4b1784fa3902d5f082b
Emanoel Johannes <emanoeljohannes@hotmail.com>
2019-09-11 19:59:18 -0300

    Versao funcionando

--- client.py
+++ client.py
@@ -1,29 +1,16 @@
-# import socket 
+import socket 
 
-# target_host = 'localhost'
-# target_port = 80
+target_host = 'localhost'
+target_port = 80
 
-# teste = 'Oi!'
+teste = 'Oi!'
 
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# client.connect((target_host, target_port))
+client.connect((target_host, target_port))
 
-# client.send(teste)
+client.send(teste)
 
-# response = client.recv(4096)
+response = client.recv(4096)
 
-# print (response)
-
-# from tkinter import *
-
-# class Application:
-#   def __init__(self, master=None):
-#       self.widget1 = Frame(master)
-#       self.widget1.pack()
-#       self.msg = Label(self.widget1, text="Primeiro widget")
-#       self.msg.pack ()
-
-# root = Tk()
-# Application(root)
-# root.mainloop()
+print (response)
